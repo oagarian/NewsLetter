8b7102c5047195ed5d70f5b06c61beb123646492
oagarian <tns6@aluno.ifal.edu.br>
2023-03-20 19:16:43 -0300

    Updating client-side files.

--- script.js
+++
@@ -1,7 +0,0 @@
-submitButton = document.getElementsByClassName("submit")[0];
-emailInput = document.getElementById("email")
-subsCounter = document.getElementsByClassName("counter")[0];
-
-submitButton.addEventListener("click", function(event) { 
-    console.log(emailInput.value);
-})
