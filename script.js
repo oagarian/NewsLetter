72c4f4a9bed29e42ef14b6a2762454074ad61246
oagarian <tns6@aluno.ifal.edu.br>
2023-03-22 17:11:09 -0300

    Finish client-side

--- script.js
+++ script.js
@@ -10,4 +10,38 @@ requestGET("http://localhost:8080/lastid").then((value) => {
   counter.textContent = value;
 })
 
+function validateEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(email);
+}
+
+function sendData(email) {
+  url = "http://localhost:8080/register"
+  requestGET("http://localhost:8080/lastid").then((value) => {
+    var atualValue = parseInt(value) + 1;
+    const data = { ID: atualValue, Email: email };
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.error(error));
+  })
+  
+}
+
+document.getElementsByClassName("submit")[0].addEventListener('click', function(){
+  inputValue = document.getElementById("email").value;
+  if (validateEmail(inputValue)) {
+    sendData(inputValue)
+    window.alert("Sucessfully submitted")
+  } else {
+    window.alert("Invalid email")
+  }
+})
 
